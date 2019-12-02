9aa164758e2d2f11c193b750c8e8a99ba980a885
Luke <deandrevidal@aol.com>
2019-12-02 10:05:39 -0800

    init

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request, redirect, url_for
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def items_index():
+    """Display home page"""
+    return render_template("home.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
