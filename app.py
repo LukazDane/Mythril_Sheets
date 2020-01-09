d61492905e17824df263e8709485c13cd8c568a0
Luke <deandrevidal@aol.com>
2020-01-08 16:45:31 -0800

    added logout and register

--- app.py
+++ app.py
@@ -1,23 +1,66 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 import os
 from config import Config
-from forms import LoginForm
+from forms import LoginForm, RegistrationForm
+from flask_login import login_required, LoginManager, current_user, login_user, logout_user
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from werkzeug.urls import url_parse
 import models
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
+
+
+#############################################
+# Login
+#############################################
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('home'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+        # return redirect(url_for('home'))
     return render_template('login.html', title='Sign In', form=form)
+
+#############################################
+# Log0ut 
+#############################################
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+#############################################
+# Register 
+#############################################
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
 
 @app.route('/')
 def home():
@@ -42,6 +85,7 @@ def library():
 
 
 @app.route('/sheets')
+@login_required
 def sheets():
     """Displays Character sheets"""
     return render_template("sheets.html")
